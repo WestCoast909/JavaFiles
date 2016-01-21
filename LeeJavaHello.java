8babb2a1f2e0e35231088ca0c74b04dc06830c4b
WestCoast909 <tldeglow@pipeline.sbcc.edu>
2016-01-21 15:12:04 -0800

    E:\JAVA\LeeJava\LeeJavaHello.java
    Testing ... Testing

---
+++ LeeJavaHello.java
@@ -0,0 +1,7 @@
+ //My first JAVA file
+package edu.sbcc.cs135;
+public class Main {
+        public static void main(String[] args) {
+                System.out.println("Hello, World");
+        }
+}
